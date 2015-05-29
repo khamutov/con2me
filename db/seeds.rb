e394793d1edcf802c8ea65cef7c6a652d3e58da5
khamutov <khamutov@ailabs.ru>
2015-05-29 15:10:10 +0300

    add data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'csv'
 
+csv_text = File.read('db/data/data.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  photo_name = row["Фото"]
+  price = row["Цена"]
+  cat_name = row["Категория"]
+  category = Category.find_by_name cat_name
+  desc = row["Описание"]
+  name = row["Имя"]
+  phone = row["Телефон"]
+
+  Offer.create!(:name => name,
+                :price => price.to_i,
+                :desc => desc,
+                :phone => phone,
+                :category => category,
+                :image => File.new("#{Rails.root}/db/data/images/#{photo_name.rjust(3, padstr='0')}.png"),
+  )
+
+  p row.to_hash
+end
